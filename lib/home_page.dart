cf2a2918812d6ab84ffb316a87758dde6e540919
Piotr Merski <merskip@gmail.com>
2020-04-17 19:17:27 +0200

    Adding sign with Google

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QWallet'),
+        title: _user(),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.exit_to_app),
@@ -17,9 +18,32 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  _user() {
+    return FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) return SizedBox.shrink();
+
+          final user = snapshot.data as FirebaseUser;
+          if (!user.isAnonymous) {
+            return Row(children: <Widget>[
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.photoUrl),
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(width: 16),
+              Text(user.displayName)
+            ]);
+          } else {
+            return Text("Hello!");
+          }
+        });
+  }
+
   _signOut() async {
     try {
       await FirebaseAuth.instance.signOut();
+      GoogleSignIn().signOut();
     } catch (e) {
       print(e); // TODO: show dialog with error
     }
--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SignInPage extends StatefulWidget {
   @override
@@ -8,13 +9,29 @@ class SignInPage extends StatefulWidget {
 
 class _SignInPageState extends State<SignInPage> {
 
+  final _googleSignIn = GoogleSignIn();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          child: Text('Sign in anonymous'),
-          onPressed: _signInAnonymous,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Welcum to QWallet!",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(height: 32),
+            RaisedButton(
+              child: Text('Sign in anonymous'), // TODO: "Continue without sing in"?
+              onPressed: _signInAnonymous,
+            ),
+            RaisedButton(
+              child: Text('Sign in with Google'),
+              onPressed: _singInWithGoogle,
+            )
+          ],
         ),
       ),
     );
@@ -24,6 +41,23 @@ class _SignInPageState extends State<SignInPage> {
     try {
       await FirebaseAuth.instance.signInAnonymously();
     } catch (e) {
+      print(e); // TODO: show dialog with error
+    }
+  }
+
+  _singInWithGoogle() async {
+    try {
+      final signInAccount = await _googleSignIn.signIn();
+      final authentication = await signInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: authentication.accessToken,
+        idToken: authentication.idToken,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    }
+    catch (e) {
       print(e); // TODO: show dialog with error
     }
   }
