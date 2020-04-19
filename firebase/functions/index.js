9b3ce97154571ac2fceb13e5c018199db7263b68
Piotr Merski <merskip@gmail.com>
2020-04-19 15:51:12 +0200

    Using property in Cloud Functions

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,22 +7,20 @@ const auth = admin.auth();
 /**
  * Gets all the users (1000 MAX) from Firebase auth.
  */
-const getAllUsers = (data, context) => {
-	console.debug("Requested users from " + context.auth.uid);
-	return auth.listUsers().then((userRecords) => {
-		const users = userRecords.users.map((user) => {
-			return {
-				uid: user.uid,
-				isAnonymous: user.email === undefined,
-				displayName: user.displayName,
-				email: user.email
-			};
-		});
-		return JSON.stringify(users);
-	}).catch((error) => {
-		console.error(error);
-		return error;
-	});
-};
-
-exports.users = functions.https.onCall(getAllUsers);
+exports.users = functions.https.onCall((data, context) => {
+    console.debug("Requested users from " + context.auth.uid);
+    return auth.listUsers().then((userRecords) => {
+        const users = userRecords.users.map((user) => {
+            return {
+                uid: user.uid,
+                isAnonymous: user.email === undefined,
+                displayName: user.displayName,
+                email: user.email
+            };
+        });
+        return JSON.stringify(users);
+    }).catch((error) => {
+        console.error(error);
+        return error;
+    });
+});
