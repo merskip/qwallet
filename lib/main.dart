e08f22cfdec53e26d620787a3a9b1f6858b3be24
Piotr Merski <merskip@gmail.com>
2020-04-19 06:53:09 +0200

    Changed primary color to brown

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "QWallet",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
       home: LandingPage(),
     );
