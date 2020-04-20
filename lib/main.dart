bfef804d592819022ba9be676ac5ee7d6827c6e6
Piotr Merski <piotr.merski@finanteq.com>
2020-04-20 12:28:57 +0200

    Darken primary color

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "QWallet",
       theme: ThemeData(
-        primarySwatch: Colors.brown,
+        primarySwatch: _darkenBrownColor(),
       ),
       debugShowCheckedModeBanner: false,
       home: LandingPage(),
     );
   }
+
+  _darkenBrownColor() => MaterialColor(
+        0xFF5D4037, // 700
+        <int, Color>{
+          50: Color(0xFFEFEBE9),
+          100: Color(0xFFD7CCC8),
+          200: Color(0xFFBCAAA4),
+          300: Color(0xFFA1887F),
+          400: Color(0xFF8D6E63),
+          500: Color(0xFF795548),
+          600: Color(0xFF6D4C41),
+          700: Color(0xFF5D4037),
+          800: Color(0xFF4E342E),
+          900: Color(0xFF3E2723),
+        },
+      );
 }
