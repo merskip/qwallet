1f6b47ad487394ee2ca4fe2a9fb20e46fa86d8a9
Piotr Merski <merskip@gmail.com>
2020-05-02 12:38:18 +0200

    Removed updateBalance function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,7 +3,6 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 const auth = admin.auth();
-const db = admin.firestore();
 
 export const getUsers = functions.https.onCall((data, context) => {
     console.debug("Requested users from " + context.auth?.uid ?? "(anonymous)");
@@ -19,29 +18,3 @@ export const getUsers = functions.https.onCall((data, context) => {
         return error;
     });
 });
-
-export const updateBalance = functions.firestore
-    .document('wallets/{walletId}/expenses/{expenseId}')
-    .onWrite((change, context) => {
-
-        console.debug("Updating wallet (" + context.params.walletId + ") balance");
-
-        const walletRef = db.collection('wallets').doc(context.params.walletId);
-        const expensesRef = walletRef.collection('expenses'); // TODO: Get only from current month
-
-        return db.runTransaction(transaction => {
-            return transaction.get(expensesRef).then(expensesDoc => {
-
-                const totalAmount = expensesDoc.docs
-                    .map(expense => expense.get("amount"))
-                    .reduce((previous, current) => previous + current, 0);
-
-                console.log("walletId=" + context.params.walletId + " Total amount: ", totalAmount);
-
-                return transaction.update(walletRef, {
-                    balance: totalAmount,
-                    isBalanceOutdated: false,
-                });
-            });
-        });
-    });
