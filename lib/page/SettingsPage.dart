bb3747899dd68f42d7fcb1dcabc6f9041ab50081
Piotr Merski <merskip@gmail.com>
2020-07-04 16:42:35 +0200

    #22 Adding "Manage your wallets" option to settings

--- lib/page/SettingsPage.dart
+++ lib/page/SettingsPage.dart
@@ -4,40 +4,53 @@ import 'package:qwallet/AppLocalizations.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatelessWidget {
+
+  onSelectedWallets(BuildContext context) {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text("TODO: Manage wallets")
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context).settings),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.language),
-            title: Text(AppLocalizations.of(context).language),
-            subtitle: Text(AppLocalizations.of(context).currentLanguage),
-          ),
-          Divider(),
-          FutureBuilder(
-            future: PackageInfo.fromPlatform(),
-            builder: (context, AsyncSnapshot<PackageInfo> info) {
-              return ListTile(
-                title: Text(AppLocalizations.of(context).version),
-                subtitle:
-                    Text("${info.data?.version} (${info.data?.buildNumber})"),
-              );
-            },
-          ),
-          ListTile(
-            title: Text("Developed by"),
-            subtitle: Text("Piotr Merski <merskip@gmail.com>\nmerskip.pl"),
-            onTap: () async {
-              const url = 'http://merskip.pl';
-              if (await canLaunch(url)) await launch(url);
-            },
-          )
-        ],
-      ),
+      body: Builder(builder: (context) {
+        return ListView(
+          children: [
+            ListTile(
+              title: Text("Wallets"),
+              subtitle: Text("Manage your wallets"),
+              onTap: () => onSelectedWallets(context),
+            ),
+            Divider(),
+            ListTile(
+              title: Text(AppLocalizations.of(context).language),
+              subtitle: Text(AppLocalizations.of(context).currentLanguage),
+            ),
+            FutureBuilder(
+              future: PackageInfo.fromPlatform(),
+              builder: (context, AsyncSnapshot<PackageInfo> info) {
+                return ListTile(
+                  title: Text(AppLocalizations.of(context).version),
+                  subtitle:
+                      Text("${info.data?.version} (${info.data?.buildNumber})"),
+                );
+              },
+            ),
+            ListTile(
+              title: Text("Developed by"),
+              subtitle: Text("Piotr Merski <merskip@gmail.com>\nmerskip.pl"),
+              onTap: () async {
+                const url = 'http://merskip.pl';
+                if (await canLaunch(url)) await launch(url);
+              },
+            )
+          ],
+        );
+      }),
     );
   }
 }
