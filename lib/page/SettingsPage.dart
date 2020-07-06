6f7ebcba04871cb4369e55f926d977b1f750cbca
Piotr Merski <merskip@gmail.com>
2020-07-06 13:10:44 +0200

    #22 Refactored settings page

--- lib/page/SettingsPage.dart
+++ lib/page/SettingsPage.dart
@@ -6,47 +6,59 @@ import 'package:url_launcher/url_launcher.dart';
 import '../router.dart';
 
 class SettingsPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context).settings),
       ),
-      body: Builder(builder: (context) {
-        return ListView(
-          children: [
-            ListTile(
-              title: Text(AppLocalizations.of(context).wallets),
-              subtitle: Text(AppLocalizations.of(context).walletsHint),
-              onTap: () => router.navigateTo(context, "/settings/wallets"),
-            ),
-            Divider(),
-            ListTile(
-              title: Text(AppLocalizations.of(context).language),
-              subtitle: Text(AppLocalizations.of(context).currentLanguage),
-            ),
-            FutureBuilder(
-              future: PackageInfo.fromPlatform(),
-              builder: (context, AsyncSnapshot<PackageInfo> info) {
-                return ListTile(
-                  title: Text(AppLocalizations.of(context).version),
-                  subtitle:
-                      Text("${info.data?.version} (${info.data?.buildNumber})"),
-                );
-              },
-            ),
-            ListTile(
-              title: Text("Developed by"),
-              subtitle: Text("Piotr Merski <merskip@gmail.com>\nmerskip.pl"),
-              onTap: () async {
-                const url = 'http://merskip.pl';
-                if (await canLaunch(url)) await launch(url);
-              },
-            )
-          ],
+      body: Builder(
+        builder: (context) => ListView(children: [
+          buildWallets(context),
+          Divider(),
+          buildLanguage(context),
+          buildApplicationVersion(),
+          buildDeveloper()
+        ]),
+      ),
+    );
+  }
+
+  Widget buildWallets(BuildContext context) {
+    return ListTile(
+      title: Text(AppLocalizations.of(context).wallets),
+      subtitle: Text(AppLocalizations.of(context).walletsHint),
+      onTap: () => router.navigateTo(context, "/settings/wallets"),
+    );
+  }
+
+  Widget buildLanguage(BuildContext context) {
+    return ListTile(
+      title: Text(AppLocalizations.of(context).language),
+      subtitle: Text(AppLocalizations.of(context).currentLanguage),
+    );
+  }
+
+  Widget buildApplicationVersion() {
+    return FutureBuilder(
+      future: PackageInfo.fromPlatform(),
+      builder: (context, AsyncSnapshot<PackageInfo> info) {
+        return ListTile(
+          title: Text(AppLocalizations.of(context).version),
+          subtitle: Text("${info.data?.version} (${info.data?.buildNumber})"),
         );
-      }),
+      },
+    );
+  }
+
+  Widget buildDeveloper() {
+    return ListTile(
+      title: Text("Developed by"),
+      subtitle: Text("Piotr Merski <merskip@gmail.com>\nmerskip.pl"),
+      onTap: () async {
+        const url = 'http://merskip.pl';
+        if (await canLaunch(url)) await launch(url);
+      },
     );
   }
 }
