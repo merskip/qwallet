5b4d4f51e408bba382dcce8458db008ca7a9dcbb
Piotr Merski <merskip@gmail.com>
2020-07-06 14:27:43 +0200

    #22 Adding remove account for non-anonymous accounts

--- lib/dialog/user_dialog.dart
+++ lib/dialog/user_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:qwallet/api/Api.dart';
 import 'package:qwallet/widget/hand_cursor.dart';
 import 'package:qwallet/widget/vector_image.dart';
 
@@ -9,18 +10,29 @@ class UserDialog extends StatelessWidget {
 
   const UserDialog({Key key, this.user}) : super(key: key);
 
-  _onSelectedSignOut(BuildContext context) async {
+  _onSelectedDeleteAccount(BuildContext context) async {
     try {
-      debugPrint("Logout...");
-      await FirebaseAuth.instance.signOut();
-      final googleSignIn = GoogleSignIn();
-      if (googleSignIn.currentUser != null) {
-        googleSignIn.signOut();
-      }
+      debugPrint("Deleting account...");
+      await Api.instance.currentUser.delete();
+
+      Navigator.of(context).popUntil(ModalRoute.withName('/'));
     } catch (e) {
       print(e); // TODO: show dialog with error
     }
-    Navigator.of(context).pop();
+  }
+
+  _onSelectedSignOut(BuildContext context) async {
+    try {
+      debugPrint("Logout...");
+      final googleSignIn = GoogleSignIn();
+      if (await googleSignIn.isSignedIn())
+        googleSignIn.signOut();
+      await FirebaseAuth.instance.signOut();
+
+      Navigator.of(context).popUntil(ModalRoute.withName('/'));
+    } catch (e) {
+      print(e); // TODO: show dialog with error
+    }
   }
 
   @override
@@ -30,6 +42,8 @@ class UserDialog extends StatelessWidget {
       children: [
         buildProvider(),
         Divider(),
+        if (!user.isAnonymous)
+          buildDeleteCurrentUser(context),
         buildLogout(context),
       ],
     );
@@ -38,23 +52,31 @@ class UserDialog extends StatelessWidget {
   Widget buildProvider() {
     return ListTile(
       title: Text("Logged with"),
+      subtitle: user.email != null ? Text(user.email) : null,
       trailing: Wrap(children: [
         if (_hasProviderId("firebase"))
           VectorImage("assets/ic-firebase-color.svg", size: Size.square(24)),
         if (_hasProviderId("google.com"))
           VectorImage("assets/ic-google-color.svg", size: Size.square(24)),
-        if (_hasProviderId("password"))
-          Icon(Icons.alternate_email, size: 24,),
+        if (_hasProviderId("password")) Icon(Icons.alternate_email, size: 24),
       ], spacing: 4),
       dense: true,
     );
   }
 
   bool _hasProviderId(String providerId) {
-    return user.providerData.firstWhere(
-      (info) => info.providerId == providerId,
-      orElse: () => null,
-    ) != null;
+    final provider = user.providerData.firstWhere(
+        (info) => info.providerId == providerId,
+        orElse: () => null);
+    return provider != null;
+  }
+
+  Widget buildDeleteCurrentUser(BuildContext context) {
+    return HandCursor(
+      child: _DeleteUserTile(
+        onConfirmed: () => _onSelectedDeleteAccount(context),
+      ),
+    );
   }
 
   Widget buildLogout(BuildContext context) {
@@ -64,5 +86,43 @@ class UserDialog extends StatelessWidget {
       title: Text("Logout"),
       onTap: () => _onSelectedSignOut(context),
     ));
+  }
+}
+
+class _DeleteUserTile extends StatefulWidget {
+  final VoidCallback onConfirmed;
+
+  const _DeleteUserTile({Key key, this.onConfirmed}) : super(key: key);
+
+  @override
+  _DeleteUserTileState createState() => _DeleteUserTileState();
+}
+
+class _DeleteUserTileState extends State<_DeleteUserTile> {
+  bool isDuringConfirm = false;
+
+  onSelected() {
+    if (!isDuringConfirm) {
+      setState(() => isDuringConfirm = true);
+    } else {
+      widget.onConfirmed();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(!isDuringConfirm ? Icons.delete_outline : Icons.delete),
+      title: Text(
+        !isDuringConfirm
+            ? "Remove account"
+            : "Are sure you want to remove account?",
+        style: TextStyle(
+            color: Colors.red,
+            fontWeight: isDuringConfirm ? FontWeight.bold : null),
+      ),
+      subtitle: isDuringConfirm ? Text("Tap here again to confirm") : null,
+      onTap: () => onSelected(),
+    );
   }
 }
