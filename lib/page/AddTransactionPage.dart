4fd4232e597c5de6dd07ba8e6bc0aafa26da2ac5
Piotr Merski <merskip@gmail.com>
2020-07-11 20:40:25 +0200

    #22 Adding form for add transaction

--- lib/page/AddTransactionPage.dart
+++ lib/page/AddTransactionPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qwallet/api/Api.dart';
 import 'package:qwallet/api/Model.dart';
 import 'package:qwallet/api/Wallet.dart';
+import 'package:qwallet/widget/PrimaryButton.dart';
 import 'package:qwallet/widget/SimpleStreamWidget.dart';
 
 class AddTransactionPage extends StatelessWidget {
@@ -29,7 +30,110 @@ class _AddTransactionPageContent extends StatelessWidget {
       appBar: AppBar(
         title: Text("#Add expense or income"),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: _AddTransactionForm(),
+        ),
+      ),
+    );
+  }
+}
+
+enum _TransactionType {
+  expense,
+  income
+}
+
+class _AddTransactionForm extends StatefulWidget {
+  @override
+  _AddTransactionFormState createState() => _AddTransactionFormState();
+}
+
+class _AddTransactionFormState extends State<_AddTransactionForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  _TransactionType type = _TransactionType.expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(children: [
+        SizedBox(height: 8),
+        buildType(context),
+        SizedBox(height: 16),
+        buildTitle(context),
+        SizedBox(height: 16),
+        buildAmount(context),
+        SizedBox(height: 16),
+        buildDate(context),
+        SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: buildSubmitButton(context),
+        )
+      ]),
+    );
+  }
+
+  Widget buildType(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+      RaisedButton(
+        child: Row(children: [
+          Icon(Icons.arrow_upward),
+          Text("Expense"),
+        ]),
+        color: type == _TransactionType.expense ? Theme.of(context).primaryColor : null,
+        textColor: type == _TransactionType.expense ? Colors.white : null,
+        onPressed: () {
+          setState(() => type = _TransactionType.expense);
+        },
+      ),
+      RaisedButton(
+        child: Row(children: [
+          Icon(Icons.arrow_downward),
+          Text("Income"),
+        ]),
+        color: type == _TransactionType.income ? Theme.of(context).primaryColor : null,
+        textColor: type == _TransactionType.income ? Colors.white : null,
+        onPressed: () {
+          setState(() => type = _TransactionType.income);
+        },
+      ),
+    ],);
+  }
+
+  Widget buildTitle(BuildContext context) {
+      return TextFormField(
+        decoration: InputDecoration(
+          labelText: "#Title",
+        ),
+      );
+  }
+
+  Widget buildAmount(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "#Amount",
+      ),
+    );
+  }
+
+  Widget buildDate(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "#Date",
+      ),
+    );
+  }
+
+  Widget buildSubmitButton(BuildContext context) {
+    return PrimaryButton(
+      child: Text("#Add"),
+      onPressed: () => {},
     );
   }
 }
