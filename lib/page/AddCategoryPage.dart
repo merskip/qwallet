823058a1cd93bf769cf336e91c37fc08700f1e38
Piotr Merski <merskip@gmail.com>
2020-07-15 17:22:54 +0200

    #22 Adding title field

--- lib/page/AddCategoryPage.dart
+++ lib/page/AddCategoryPage.dart
@@ -3,7 +3,6 @@ import 'package:qwallet/api/Model.dart';
 import 'package:qwallet/api/Wallet.dart';
 
 class AddCategoryPage extends StatelessWidget {
-
   final Reference<Wallet> walletRef;
 
   const AddCategoryPage({Key key, this.walletRef}) : super(key: key);
@@ -14,6 +13,43 @@ class AddCategoryPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("#Add category"),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: _AddCategoryForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class _AddCategoryForm extends StatefulWidget {
+  @override
+  _AddCategoryFormState createState() => _AddCategoryFormState();
+}
+
+class _AddCategoryFormState extends State<_AddCategoryForm> {
+  final _formKey = GlobalKey<_AddCategoryFormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(children: [
+        buildTitleField(context),
+      ]),
+    );
+  }
+
+  Widget buildTitleField(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "#Title",
+      ),
+      autofocus: true,
+      maxLength: 50,
+      textCapitalization: TextCapitalization.sentences,
+      textInputAction: TextInputAction.next,
     );
   }
 }
