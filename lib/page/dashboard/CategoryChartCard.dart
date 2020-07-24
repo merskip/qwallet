6cdfb2b7d80318f95f5630fae34daee1913577ef
Piotr Merski <merskip@gmail.com>
2020-07-24 11:10:57 +0200

    #22 Extracted categories chart into _CategoriesChart

--- lib/page/dashboard/CategoryChartCard.dart
+++ lib/page/dashboard/CategoryChartCard.dart
@@ -3,7 +3,6 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:qwallet/api/Category.dart';
 import 'package:qwallet/api/DataSource.dart';
-import 'package:qwallet/api/Model.dart';
 import 'package:qwallet/api/Transaction.dart';
 import 'package:qwallet/api/Wallet.dart';
 import 'package:qwallet/widget/SimpleStreamWidget.dart';
@@ -57,7 +56,7 @@ class CategoryChartCard extends StatelessWidget {
         Stack(
           alignment: Alignment.center,
           children: [
-            buildPieChart(context, transactions, categories),
+            buildChart(context, transactions, categories),
             Column(children: [
               Text(
                 Money(wallet.totalExpense, wallet.currency).formatted,
@@ -74,56 +73,84 @@ class CategoryChartCard extends StatelessWidget {
     );
   }
 
-  Widget buildPieChart(
-    BuildContext context,
-    List<Transaction> transactions,
-    List<Category> categories,
-  ) {
-    final allExpenses =
-        transactions.where((t) => t.type == TransactionType.expense);
-    final transactionsByCategory =
-        groupBy(allExpenses, (Transaction t) => t.category);
+  Widget buildChart(
+      BuildContext context,
+      List<Transaction> transactions,
+      List<Category> categories,) {
+    final allExpenses = transactions.where((t) => t.type == TransactionType.expense);
+    final transactionsByCategory = groupBy(allExpenses, (Transaction t) => t.category);
 
-    if (allExpenses.isEmpty) return Container();
-    return PieChart(
-      PieChartData(
-        sections: [
-          ...transactionsByCategory.keys.map((categoryRef) => createSection(
-                context,
-                transactionsByCategory,
-                categories,
-                categoryRef,
-              )),
-        ],
-        borderData: FlBorderData(show: false),
-      ),
-    );
+    final categoryChartItems = transactionsByCategory.keys.map((categoryRef) {
+      final category = categories.firstWhere(
+        (c) => c.reference.id == categoryRef?.id,
+        orElse: () => null,
+      );
+      final transactions = transactionsByCategory[categoryRef];
+
+      return _CategoryChartItem(wallet, category, transactions);
+    }).toList();
+
+    return _CategoriesChart(items: categoryChartItems);
+  }
+
+}
+
+class _CategoriesChart extends StatefulWidget {
+  final List<_CategoryChartItem> items;
+
+  final double totalAmount;
+
+  _CategoriesChart({Key key, this.items})
+      : totalAmount = items.fold(0.0, (acc, i) => acc + i.sum.amount),
+        super(key: key);
+
+  @override
+  _CategoriesChartState createState() => _CategoriesChartState();
+}
+
+class _CategoriesChartState extends State<_CategoriesChart> {
+  @override
+  Widget build(BuildContext context) {
+    if (widget.items.isEmpty)
+      return Container();
+    else
+      return PieChart(
+        PieChartData(
+          sections: [
+            ...widget.items.map((item) => createSection(context, item)),
+          ],
+          borderData: FlBorderData(show: false),
+        ),
+      );
   }
 
   PieChartSectionData createSection(
     BuildContext context,
-    Map<Reference<Category>, List<Transaction>> transactionsByCategory,
-    List<Category> categories,
-    Reference<Category> categoryRef,
+    _CategoryChartItem item,
   ) {
-    final category = categories.firstWhere(
-      (c) => c.reference.id == categoryRef?.id,
-      orElse: () => null,
-    );
-    final expenses = transactionsByCategory[categoryRef];
-
-    final double totalAmount = expenses.fold(
-        0.0, (previousValue, element) => previousValue + element.amount);
-
-    final percentage = (totalAmount / wallet.totalExpense * 100).round();
+    final percentage = (item.sum.amount / widget.totalAmount * 100).round();
+    final titleStyle = Theme.of(context).textTheme.bodyText1.copyWith(
+          backgroundColor: item.category?.backgroundColor ?? Colors.grey,
+        );
 
     return PieChartSectionData(
-      value: totalAmount,
-      color: category?.primaryColor ?? Colors.black12,
-      title: (category?.title ?? "#No category") + " ($percentage%)",
-      titleStyle: Theme.of(context).textTheme.bodyText1.copyWith(
-          backgroundColor: category?.backgroundColor ?? Colors.grey),
+      value: item.sum.amount,
+      color: item.category?.primaryColor ?? Colors.black12,
+      title: (item.category?.title ?? "#No category") + " ($percentage%)",
+      titleStyle: titleStyle,
       radius: 48,
     );
   }
+}
+
+class _CategoryChartItem {
+  final Wallet wallet;
+  final Category category;
+  final List<Transaction> transactions;
+
+  Money get sum => Money(
+      transactions.fold(0.0, (amount, t) => amount + t.amount),
+      wallet.currency);
+
+  _CategoryChartItem(this.wallet, this.category, this.transactions);
 }
