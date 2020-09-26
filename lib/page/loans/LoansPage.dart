862e879d7e736c3fbaed208ed03cfddf09f27311
Piotr Merski <merskip@gmail.com>
2020-09-26 14:00:57 +0200

    #23 Extracted class LoansGroupCard

--- lib/page/loans/LoansPage.dart
+++ lib/page/loans/LoansPage.dart
@@ -17,10 +17,6 @@ class LoansPage extends StatefulWidget {
 }
 
 class _LoansPageState extends State<LoansPage> {
-  void onSelectedLoan(BuildContext context, PrivateLoan loan) {
-    router.navigateTo(context, "/privateLoans/${loan.id}/edit");
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,7 +50,7 @@ class _LoansPageState extends State<LoansPage> {
     return ListView.builder(
       itemCount: groups.length,
       itemBuilder: (context, index) =>
-          buildLoansGroupItem(context, groups[index]),
+          LoansGroupCard(loansGroup: groups[index]),
       padding: const EdgeInsets.only(bottom: 96),
     );
   }
@@ -82,118 +78,6 @@ class _LoansPageState extends State<LoansPage> {
     return groups
       ..forEach((g) => g.finalize())
       ..sort();
-  }
-
-  Widget buildLoansGroupItem(BuildContext context, _LoansGroup loansGroup) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Column(children: [
-        buildLoansGroupHeader(context, loansGroup),
-        Divider(),
-        for (final loan in loansGroup.loans) buildLoanItem(context, loan)
-      ]),
-    );
-  }
-
-  Widget buildLoansGroupHeader(BuildContext context, _LoansGroup loansGroup) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              if (loansGroup.otherPersonUser != null)
-                Padding(
-                  padding: const EdgeInsets.only(right: 12),
-                  child: UserAvatar(user: loansGroup.otherPersonUser),
-                ),
-              Text(
-                loansGroup.getOtherPersonCommonName(context),
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ],
-          ),
-          SizedBox(height: 8),
-          if (loansGroup.debtOfOtherPerson.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: buildLoanGroupSummaryItem(
-                context,
-                title: Text("#Dług"),
-                values: loansGroup.debtOfOtherPerson,
-              ),
-            ),
-          if (loansGroup.debtOfCurrentUser.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: buildLoanGroupSummaryItem(
-                context,
-                title: Text("#Mój dług"),
-                values: loansGroup.debtOfCurrentUser,
-                defaultStyle: TextStyle(color: Colors.red),
-              ),
-            ),
-          Row(children: [
-            Flexible(flex: 2, child: Container()),
-            Flexible(child: Divider())
-          ]),
-          buildLoanGroupSummaryItem(
-            context,
-            values: loansGroup.balance,
-            defaultStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildLoanGroupSummaryItem(
-    BuildContext context, {
-    Widget title,
-    List<Money> values,
-    TextStyle defaultStyle,
-  }) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (title != null) title else Container(),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ...values.map((money) {
-              return Text(
-                money.formatted,
-                style: (defaultStyle ?? TextStyle()).copyWith(
-                  color: money.amount.isNegative ? Colors.red : null,
-                  // fontSize: 15,
-                  // fontWeight: FontWeight.w500,
-                ),
-              );
-            }),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget buildLoanItem(BuildContext context, PrivateLoan loan) {
-    final locale = AppLocalizations.of(context).locale.toString();
-    final format = DateFormat("d MMMM yyyy", locale);
-    return ListTile(
-      title: Text(loan.title),
-      subtitle: Text(format.format(loan.date)),
-      trailing: Text(
-        loan.remainingAmount.formatted,
-        style: TextStyle(
-          color: loan.currentUserIsBorrower ? Colors.red : null,
-        ),
-      ),
-      onTap: () => onSelectedLoan(context, loan),
-      dense: true,
-      visualDensity: VisualDensity.compact,
-    );
   }
 }
 
@@ -249,5 +133,133 @@ class _LoansGroup implements Comparable {
     } else {
       return 0;
     }
+  }
+}
+
+class LoansGroupCard extends StatefulWidget {
+  final _LoansGroup loansGroup;
+
+  const LoansGroupCard({Key key, this.loansGroup}) : super(key: key);
+
+  @override
+  _LoansGroupCardState createState() => _LoansGroupCardState();
+}
+
+class _LoansGroupCardState extends State<LoansGroupCard> {
+  void onSelectedLoan(BuildContext context, PrivateLoan loan) {
+    router.navigateTo(context, "/privateLoans/${loan.id}/edit");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Column(children: [
+        buildHeader(context, widget.loansGroup),
+        Divider(),
+        for (final loan in widget.loansGroup.loans) buildLoanItem(context, loan)
+      ]),
+    );
+  }
+
+  Widget buildHeader(BuildContext context, _LoansGroup loansGroup) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              if (loansGroup.otherPersonUser != null)
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: UserAvatar(user: loansGroup.otherPersonUser),
+                ),
+              Text(
+                loansGroup.getOtherPersonCommonName(context),
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          if (loansGroup.debtOfOtherPerson.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: buildSummaryItem(
+                context,
+                title: Text("#Dług"),
+                values: loansGroup.debtOfOtherPerson,
+              ),
+            ),
+          if (loansGroup.debtOfCurrentUser.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: buildSummaryItem(
+                context,
+                title: Text("#Mój dług"),
+                values: loansGroup.debtOfCurrentUser,
+                defaultStyle: TextStyle(color: Colors.red),
+              ),
+            ),
+          Row(children: [
+            Flexible(flex: 2, child: Container()),
+            Flexible(child: Divider())
+          ]),
+          buildSummaryItem(
+            context,
+            values: loansGroup.balance,
+            defaultStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildSummaryItem(
+    BuildContext context, {
+    Widget title,
+    List<Money> values,
+    TextStyle defaultStyle,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (title != null) title else Container(),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ...values.map((money) {
+              return Text(
+                money.formatted,
+                style: (defaultStyle ?? TextStyle()).copyWith(
+                  color: money.amount.isNegative ? Colors.red : null,
+                  // fontSize: 15,
+                  // fontWeight: FontWeight.w500,
+                ),
+              );
+            }),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget buildLoanItem(BuildContext context, PrivateLoan loan) {
+    final locale = AppLocalizations.of(context).locale.toString();
+    final format = DateFormat("d MMMM yyyy", locale);
+    return ListTile(
+      title: Text(loan.title),
+      subtitle: Text(format.format(loan.date)),
+      trailing: Text(
+        loan.remainingAmount.formatted,
+        style: TextStyle(
+          color: loan.currentUserIsBorrower ? Colors.red : null,
+        ),
+      ),
+      onTap: () => onSelectedLoan(context, loan),
+      dense: true,
+      visualDensity: VisualDensity.compact,
+    );
   }
 }
