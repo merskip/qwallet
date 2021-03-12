70ca2ac7de38274d59aba1568ed526123abd289a
Piotr Merski <merskip@gmail.com>
2021-03-12 06:10:07 +0100

    Fixed vector images

--- lib/widget/vector_image.dart
+++ lib/widget/vector_image.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class VectorImage extends StatelessWidget {
   final String assetName;
@@ -11,22 +12,20 @@ class VectorImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Fix this
-    // if (kIsWeb) {
-    //   return Image.network(
-    //     "/assets/$assetName",
-    //     width: size?.width,
-    //     height: size?.height,
-    //     color: color,
-    //   );
-    // } else {
-    //   return SvgPicture.asset(
-    //     assetName,
-    //     width: size?.width,
-    //     height: size?.height,
-    //     color: color,
-    //   );
-    // }
-    return Text(assetName);
+    if (kIsWeb) {
+      return Image.network(
+        "/assets/$assetName",
+        width: size?.width,
+        height: size?.height,
+        color: color,
+      );
+    } else {
+      return SvgPicture.asset(
+        assetName,
+        width: size?.width,
+        height: size?.height,
+        color: color,
+      );
+    }
   }
 }
