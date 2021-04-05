034029d48fb417a46b0ebbb4be341b961d22ce27
Piotr Merski <merskip@gmail.com>
2021-04-05 12:35:07 +0200

    #48 Using category color by row index

--- lib/datasource/google_sheets/SpreadsheetWalletsProvider.dart
+++ lib/datasource/google_sheets/SpreadsheetWalletsProvider.dart
@@ -86,13 +86,34 @@ class SpreadsheetWalletsProvider implements WalletsProvider {
   }
 
   SpreadsheetCategory _toCategory(GoogleSpreadsheetCategory category) {
+    final categoriesColors = <MaterialColor>[
+      Colors.red,
+      Colors.pink,
+      Colors.purple,
+      Colors.deepPurple,
+      Colors.indigo,
+      Colors.blue,
+      Colors.lightBlue,
+      Colors.cyan,
+      Colors.teal,
+      Colors.green,
+      Colors.lightGreen,
+      Colors.lime,
+      Colors.yellow,
+      Colors.amber,
+      Colors.orange,
+      Colors.deepOrange,
+      Colors.brown
+    ];
+    final primaryColor =
+        categoriesColors[category.row % categoriesColors.length];
     return SpreadsheetCategory(
       identifier:
           Identifier(domain: "google_sheets", id: category.row.toString()),
       symbol: category.symbol,
       title: category.description,
-      primaryColor: null,
-      backgroundColor: null,
+      primaryColor: primaryColor.shade800,
+      backgroundColor: primaryColor.shade100,
       order: category.row,
     );
   }
