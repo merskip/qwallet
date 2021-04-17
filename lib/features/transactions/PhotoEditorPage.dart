7438c0e4369690112ddb7b77bec98f21388ddbc6
Piotr Merski <merskip@gmail.com>
2021-04-17 08:50:34 +0200

    Adding CropState class

--- lib/features/transactions/PhotoEditorPage.dart
+++ lib/features/transactions/PhotoEditorPage.dart
@@ -18,16 +18,8 @@ class PhotoEditorPage extends StatefulWidget {
 
 class _PhotoEditorPageState extends State<PhotoEditorPage> {
   ui.Image? image;
-  Rect imageRect = Rect.zero;
   double rotation = 0;
-  Rect crop = Rect.zero;
-  Rect draggingCrop = Rect.zero;
-
-  var isDraggingLeft = false;
-  var isDraggingTop = false;
-  var isDraggingRight = false;
-  var isDraggingBottom = false;
-  var isDraggingOffset = false;
+  late CropState cropState;
 
   @override
   void initState() {
@@ -40,78 +32,14 @@ class _PhotoEditorPageState extends State<PhotoEditorPage> {
     ui.decodeImageFromList(bytes, (image) {
       setState(() {
         this.image = image;
-        imageRect = Rect.fromLTWH(
+        final imageRect = Rect.fromLTWH(
           0.0,
           0.0,
           image.width.toDouble(),
           image.height.toDouble(),
         );
-        this.crop = imageRect;
-        this.draggingCrop = imageRect;
+        cropState = CropState(imageRect);
       });
-    });
-  }
-
-  void onPanStart(DragStartDetails details) {
-    if ((details.localPosition.dx - crop.left).abs() < 36) {
-      isDraggingLeft = true;
-    }
-    if ((details.localPosition.dy - crop.top).abs() < 36) {
-      isDraggingTop = true;
-    }
-    if ((details.localPosition.dx - crop.right).abs() < 36) {
-      isDraggingRight = true;
-    }
-    if ((details.localPosition.dy - crop.bottom).abs() < 36) {
-      isDraggingBottom = true;
-    }
-    if (crop.contains(details.localPosition) &&
-        !isDraggingLeft &&
-        !isDraggingTop &&
-        !isDraggingRight &&
-        !isDraggingBottom) {
-      isDraggingOffset = true;
-    }
-    print("Start pan with "
-        "isDraggingLeft=$isDraggingLeft, "
-        "isDraggingTop=$isDraggingTop, "
-        "isDraggingRight=$isDraggingRight, "
-        "isDraggingBottom=$isDraggingBottom, "
-        "isDraggingOffset=$isDraggingOffset");
-  }
-
-  void onPanUpdate(DragUpdateDetails details) {
-    var crop = this.draggingCrop;
-    if (isDraggingLeft)
-      crop = Rect.fromLTRB(
-          crop.left + details.delta.dx, crop.top, crop.right, crop.bottom);
-    if (isDraggingTop)
-      crop = Rect.fromLTRB(
-          crop.left, crop.top + details.delta.dy, crop.right, crop.bottom);
-    if (isDraggingRight)
-      crop = Rect.fromLTRB(
-          crop.left, crop.top, crop.right + details.delta.dx, crop.bottom);
-    if (isDraggingBottom)
-      crop = Rect.fromLTRB(
-          crop.left, crop.top, crop.right, crop.bottom + details.delta.dy);
-    if (isDraggingOffset) crop = crop.shift(details.delta);
-
-    if (crop != this.draggingCrop) {
-      setState(() {
-        this.draggingCrop = crop;
-        this.crop = imageRect.intersect(crop);
-      });
-    }
-  }
-
-  void onPanEnd(DragEndDetails details) {
-    isDraggingLeft = false;
-    isDraggingTop = false;
-    isDraggingRight = false;
-    isDraggingBottom = false;
-    isDraggingOffset = false;
-    setState(() {
-      this.draggingCrop = crop;
     });
   }
 
@@ -138,16 +66,22 @@ class _PhotoEditorPageState extends State<PhotoEditorPage> {
               width: image.width.toDouble(),
               height: image.height.toDouble(),
               child: GestureDetector(
-                onPanStart: onPanStart,
-                onPanUpdate: onPanUpdate,
-                onPanEnd: onPanEnd,
+                onPanStart: (details) => setState(() {
+                  cropState = cropState.panStart(details);
+                }),
+                onPanUpdate: (details) => setState(() {
+                  cropState = cropState.panUpdate(details);
+                }),
+                onPanEnd: (details) => setState(() {
+                  cropState = cropState.panEnd();
+                }),
                 child: CustomPaint(
                   painter: _ImagePainter(
                     image: image,
                     rotate: rotation,
                   ),
                   foregroundPainter: _CropPainter(
-                    crop: crop,
+                    cropState: cropState,
                   ),
                 ),
               ),
@@ -203,14 +137,16 @@ class _ImagePainter extends CustomPainter {
 }
 
 class _CropPainter extends CustomPainter {
-  final Rect crop;
+  final CropState cropState;
 
   _CropPainter({
-    required this.crop,
+    required this.cropState,
   });
 
   @override
   void paint(Canvas canvas, ui.Size size) {
+    final crop = cropState.crop;
+
     final dimmingPaint = Paint()..color = Colors.black54;
     canvas.drawRect(Rect.fromLTWH(0, 0, size.width, crop.top), dimmingPaint);
     canvas.drawRect(
@@ -238,6 +174,121 @@ class _CropPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant _CropPainter oldDelegate) {
-    return crop != oldDelegate.crop;
+    return cropState != oldDelegate.cropState;
   }
+}
+
+class CropState {
+  final Rect crop;
+  final Rect draggingCrop;
+  final Rect imageRect;
+
+  final bool isDraggingLeft;
+  final bool isDraggingTop;
+  final bool isDraggingRight;
+  final bool isDraggingBottom;
+  final bool isDraggingOffset;
+
+  final dragRadius = 36.0;
+
+  CropState(Rect imageRect)
+      : crop = imageRect,
+        draggingCrop = imageRect,
+        imageRect = imageRect,
+        isDraggingLeft = false,
+        isDraggingTop = false,
+        isDraggingRight = false,
+        isDraggingBottom = false,
+        isDraggingOffset = false;
+
+  CropState._(
+    this.crop,
+    this.draggingCrop,
+    this.imageRect,
+    this.isDraggingLeft,
+    this.isDraggingTop,
+    this.isDraggingRight,
+    this.isDraggingBottom,
+    this.isDraggingOffset,
+  );
+
+  CropState panStart(DragStartDetails details) {
+    final dx = details.localPosition.dx;
+    final dy = details.localPosition.dy;
+    final isDraggingLeft = (dx - crop.left).abs() < dragRadius;
+    final isDraggingTop = (dy - crop.top).abs() < dragRadius;
+    final isDraggingRight = (dx - crop.right).abs() < dragRadius;
+    final isDraggingBottom = (dy - crop.bottom).abs() < dragRadius;
+    final isDraggingOffset = crop.contains(details.localPosition) &&
+        !isDraggingLeft &&
+        !isDraggingTop &&
+        !isDraggingRight &&
+        !isDraggingBottom;
+
+    print("Pan start: "
+        "left=$isDraggingLeft, "
+        "top=$isDraggingTop, "
+        "right=$isDraggingRight, "
+        "bottom=$isDraggingBottom, "
+        "offset=$isDraggingOffset");
+    return _copy(
+      isDraggingLeft: isDraggingLeft,
+      isDraggingTop: isDraggingTop,
+      isDraggingRight: isDraggingRight,
+      isDraggingBottom: isDraggingBottom,
+      isDraggingOffset: isDraggingOffset,
+    );
+  }
+
+  CropState panUpdate(DragUpdateDetails details) {
+    var crop = this.draggingCrop;
+    if (isDraggingLeft)
+      crop = Rect.fromLTRB(
+          crop.left + details.delta.dx, crop.top, crop.right, crop.bottom);
+    if (isDraggingTop)
+      crop = Rect.fromLTRB(
+          crop.left, crop.top + details.delta.dy, crop.right, crop.bottom);
+    if (isDraggingRight)
+      crop = Rect.fromLTRB(
+          crop.left, crop.top, crop.right + details.delta.dx, crop.bottom);
+    if (isDraggingBottom)
+      crop = Rect.fromLTRB(
+          crop.left, crop.top, crop.right, crop.bottom + details.delta.dy);
+    if (isDraggingOffset) crop = crop.shift(details.delta);
+
+    return _copy(
+      draggingCrop: crop,
+      crop: imageRect.intersect(crop),
+    );
+  }
+
+  CropState panEnd() => _copy(
+        draggingCrop: crop,
+        isDraggingLeft: false,
+        isDraggingTop: false,
+        isDraggingRight: false,
+        isDraggingBottom: false,
+        isDraggingOffset: false,
+      );
+
+  CropState _copy({
+    Rect? crop,
+    Rect? draggingCrop,
+    Rect? imageRect,
+    bool? isDraggingLeft,
+    bool? isDraggingTop,
+    bool? isDraggingRight,
+    bool? isDraggingBottom,
+    bool? isDraggingOffset,
+  }) =>
+      CropState._(
+        crop ?? this.crop,
+        draggingCrop ?? this.draggingCrop,
+        imageRect ?? this.imageRect,
+        isDraggingLeft ?? this.isDraggingLeft,
+        isDraggingTop ?? this.isDraggingTop,
+        isDraggingRight ?? this.isDraggingRight,
+        isDraggingBottom ?? this.isDraggingBottom,
+        isDraggingOffset ?? this.isDraggingOffset,
+      );
 }
